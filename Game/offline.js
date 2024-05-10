30671eaf6ee96f7085cb7aa737c0e77b3778a9cc
krpalmy <plamykrittya@gmail.com>
2024-05-10 17:06:44 +0700

    Games

---
+++ Game/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1715335155,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/sprite1-sheet0.png",
+		"images/lef-sheet0.png",
+		"images/rig-sheet0.png",
+		"images/bom-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
